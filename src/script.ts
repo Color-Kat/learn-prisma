91955b6a789e080cb48e3c333a8c7295d6c642d3
Color-Kat <katik7979@gmail.com>
2024-01-19 12:25:13 +0300

    increment

--- src/script.ts
+++ src/script.ts
@@ -3,40 +3,42 @@ import {PrismaClient} from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.user.deleteMany();
-    const user = await prisma.user.create({
-        data: {
-            name: "ColorKat2",
-            email: "coco@co.co",
-            age: 18,
-            userPreference: {
-                create: {
-                    emailNews: true
-                }
-            }
-        },
-        select: {
-            name: true,
-            userPreference: { select: { emailNews: true } }
-        }
-    });
+    // await prisma.user.deleteMany();
+    // const user = await prisma.user.create({
+    //     data: {
+    //         name: "ColorKat2",
+    //         email: "coco@co.co",
+    //         age: 18,
+    //         userPreference: {
+    //             create: {
+    //                 emailNews: true
+    //             }
+    //         }
+    //     },
+    //     select: {
+    //         name: true,
+    //         userPreference: { select: { emailNews: true } }
+    //     }
+    // });
 
-    const usersCount = await prisma.user.createMany({
-        data: [
-            {name: "Admin", age: 10, email: "admin@admin.admin"},
-            {name: "Masha", age: 17, email: "maria@mail.ru"},
-            {name: "Gustov", age: 34, email: "gustov@han.hrestian"},
-        ],
-    });
+    // const usersCount = await prisma.user.createMany({
+    //     data: [
+    //         {name: "Admin", age: 10, email: "admin@admin.admin"},
+    //         {name: "Masha", age: 17, email: "maria@mail.ru"},
+    //         {name: "Gustov", age: 34, email: "gustov@han.hrestian"},
+    //     ],
+    // });
 
-    console.log(usersCount);
+    // console.log(usersCount);
 
     const updatedUser = await prisma.user.update({
        where: {
            email: "admin@admin.admin",
        },
         data: {
-            age: 20
+            age: {
+                increment: 1
+            }
         }
     });
 
